1b055e5d9558c43128d98cc1cfb554d974ab6f59
Artur Kuznetsov <arsbroart@mail.ru>
2024-03-14 16:01:37 +0300

    feat: extend SurveyEditRequest from SurveyCreationRequest

--- SurveyPlatform/src/Application/SurveyPlatform.Application.Contracts/Models/SurveyEditRequest.cs
+++ SurveyPlatform/src/Application/SurveyPlatform.Application.Contracts/Models/SurveyEditRequest.cs
@@ -1,20 +1,10 @@
-﻿using System.Collections.ObjectModel;
-
-namespace SurveyPlatform.Application.Contracts.Models;
+﻿namespace SurveyPlatform.Application.Contracts.Models;
 public abstract class SurveyEditRequest(
     int surveyId,
     int statusId,
     string title,
     string description,
-    IList<QuestionRequest> questions)
+    IList<QuestionRequest> questions) : SurveyCreationRequest(statusId, title, description, questions)
 {
     public int SurveyId { get; } = surveyId;
-
-    public int StatusId { get; } = statusId;
-
-    public string Title { get; } = title;
-
-    public string Description { get; } = description;
-
-    public IReadOnlyCollection<QuestionRequest> Questions { get; } = new ReadOnlyCollection<QuestionRequest>(questions);
 }
