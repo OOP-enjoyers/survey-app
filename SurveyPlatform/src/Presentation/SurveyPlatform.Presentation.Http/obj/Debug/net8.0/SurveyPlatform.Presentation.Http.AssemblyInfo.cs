9f2ef995628b8f642b145fecb00216194649f896
Artur Kuznetsov <arsbroart@mail.ru>
2024-03-14 17:31:57 +0300

    additional files

--- SurveyPlatform/src/Presentation/SurveyPlatform.Presentation.Http/obj/Debug/net8.0/SurveyPlatform.Presentation.Http.AssemblyInfo.cs
+++ SurveyPlatform/src/Presentation/SurveyPlatform.Presentation.Http/obj/Debug/net8.0/SurveyPlatform.Presentation.Http.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyPlatform.Presentation.Http")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a62fffae7b0de38b779cd6d8b7b326655718897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c964b558aa31e83d0f96a65b01df13a86d5bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyPlatform.Presentation.Http")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyPlatform.Presentation.Http")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
