eec053865e176625ed966b62e55e38d42343b3b2
Artur Kuznetsov <arsbroart@mail.ru>
2024-03-19 23:15:53 +0300

    refactor: update Response model to use userId and questionId

--- SurveyPlatform/src/Application/SurveyPlatform.Application.Models/Models/Response.cs
+++ SurveyPlatform/src/Application/SurveyPlatform.Application.Models/Models/Response.cs
@@ -3,14 +3,14 @@
 public class Response(
     int id,
     IReadOnlyCollection<string> content,
-    User user,
-    Question question)
+    int userId,
+    int questionId)
 {
     public int Id { get; private set; } = id;
 
     public IReadOnlyCollection<string> Content { get; private set; } = content;
 
-    public User User { get; set; } = user;
+    public int UserId { get; set; } = userId;
 
-    public Question Question { get; set; } = question;
+    public int QuestionId { get; set; } = questionId;
 }
